018ffdc5d50541bc7216fd70af7cc91b2aca1150
kasp4770 <kasp4770@edu.easj.dk>
2018-03-19 22:07:48 +0100

    added test class

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,16 +1,20 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 public class VendingMachine {
 
     //create an object of Singleton
     private static VendingMachine instance = new VendingMachine();
+
     private static ArrayList<Item> pantlist;
+    private Date date;
     private static double countMonays = 0.0;
+    private static int countItems = 0;
 
     //Constructor is private so it can not be intantiated
     private VendingMachine(){
         pantlist = new ArrayList<>();
+        date = new Date();
     }
 
     public static VendingMachine getInstance(){
@@ -24,15 +28,29 @@ public class VendingMachine {
     public void add(Item item){
         pantlist.add(item);
         countMonays = countMonays + item.getPrice();
-
+        countItems++;
+    }
+    public void clear(){
+        pantlist.clear();
+        countMonays = 0.0;
+        countItems = 0;
     }
 
     public double getCountMonays(){
         return countMonays;
     }
 
+    public int getCountItems(){
+        return countItems;
+    }
+
+    public void printReciept(){
+        System.out.println("Date: " + date.toString() + "\nCans'n'Bottles: " + countItems + "\nMonays: " + getCountMonays());
+        clear();
+    }
+
     public void showMessage(){
-        System.out.println("This is a vendingmachine. Insert your cans or bottles, and retrive monays for it.");
+        System.out.println("This is a vendingmachine. Insert your cans or bottles, and retrieve monays for it.");
     }
 
 }
--- src/VendingMachineTest.java
+++ src/VendingMachineTest.java
@@ -1,19 +1,31 @@
 public class VendingMachineTest {
     public static void main(String[] args) {
 
+        //Get Singleton object
         VendingMachine object = VendingMachine.getInstance();
 
+        //Add items to the list of the VendingMachine object.
         Item item1 = new Item(Types.A, 1.0);
         Item item2 = new Item(Types.B, 1.5);
         Item item3 = new Item(Types.C, 3.0);
-
         object.add(item1);
         object.add(item2);
         object.add(item3);
 
+        //test of Item toString() method.
         System.out.println(object.getPantlist());
 
-        System.out.println("\n" + object.getCountMonays());
+        //test of static datafields
+        System.out.println("\n" + object.getCountItems());
+        System.out.println(object.getCountMonays());
+
+        //Reciept
+        object.printReciept();
+
+        //Test that the printReciept() method cleared the pantlist and reset the static datafields.
+        System.out.println("\n" + object.getPantlist());
+        System.out.println(object.getCountMonays());
+        System.out.println(object.getCountItems());
 
     }
 }
